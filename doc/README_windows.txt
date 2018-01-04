4b780a38a6678ec2a8ef4743fc50e7438e8f3196
Joseph Chen <aoxiang.chen@ele.me>
2018-01-04 12:11:52 +0800

    code submitted to public accessible repos

---
+++ doc/README_windows.txt
@@ -0,0 +1,23 @@
+nToken Core 0.12.1
+=====================
+
+Intro
+-----
+nToken is a free open source peer-to-peer electronic cash system that is
+completely decentralized, without the need for a central server or trusted
+parties.  Users hold the crypto keys to their own money and transact directly
+with each other, with the help of a P2P network to check for double-spending.
+
+
+Setup
+-----
+Unpack the files into a directory and run ntoken-qt.exe.
+
+nToken Core is the original nToken client and it builds the backbone of the network.
+However, it downloads and stores the entire history of nToken transactions;
+depending on the speed of your computer and network connection, the synchronization
+process can take anywhere from a few hours to a day or more.
+
+See the ntoken wiki at:
+  https://ntoken.atlassian.net/wiki/
+for more help and information.
---
+++ src/drafted/governance-categories.h
@@ -0,0 +1,58 @@
+
+/*
+	CATEGORY MAPPING
+
+	* means the category has an associated class
+	
+	CnTokenNetwork: NTOKEN NETWORK (ROOT)
+	has:
+		vector<CNetworkVariable> vecNetworkVariables;
+		vector<CnTokenProject> vecProjects;
+		vector<CGovernanceObject> vecProposals;
+		vector<CBudgetContract> vecContracts;
+		vector<CBudgetUsers> vecUsers;
+
+	CnTokenProject:
+	has:
+		name
+		employees
+
+*/
+
+	// NTOKEN NETWORK (ROOT)
+	// 	-> NETWORK VARIABLE
+	// 		-> switch, setting
+	// 	-> CATEGORIES
+	// 		-> LEVEL
+	// 			-> I, II, III, IV, V, VI, VII, VIII, IX, X, XI
+	// 		-> VALUEOVERRIDE
+	// 			-> NETWORK, OWNER 
+	// 		-> PROJECT*
+	// 			-> TYPES
+	// 				-> SOFTWARE
+	// 					-> CORE, NONCORE
+	// 				-> HARDWARE
+	// 				-> PR
+	// 			-> PROJECT REPORT*
+	// 				-> UPDATE
+	// 			-> PROJECT MILESTONE*
+	// 				-> START, ONGOING, COMPLETE, FAILURE
+	// 			-> PROPOSAL*
+	// 				-> FUNDING, GOVERNANCE, AMEND, GENERIC
+	// 			-> CONTRACT*
+	// 				-> TYPE
+	// 					-> INTERNAL, EXTERNAL
+	// 				-> STATUS
+	// 					-> OK
+	// 	-> GROUPS
+	// 		-> GROUP1
+	// 			-> USER1 (only users are allowed here in this scope)
+	// 			-> USER2
+	// 		-> GROUP2 (EVO)
+	// 			-> VALUEOVERRIDE (STORE=GBXDRIVE)
+	// 			-> USER1
+
+	// 	-> COMPANIES
+	// 		-> COMPANY1
+	// 		-> DAO1
+
